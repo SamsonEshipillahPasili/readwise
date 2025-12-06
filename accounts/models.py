96964cd0332e1127cc123ef61498a3bd1139e06b
Eshipillah Pasili Samson <sameshipilla94@gmail.com>
2025-12-06 19:50:57 +0300

    Add a custom user model for the ReadWise users, to allow for future extensions

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,7 @@
-from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class ReadWiseUser(AbstractUser):
+    """
+    This is the custom user model for all ReadWise users.
+    """
+    ...
