9b0aa289a9420734b63247a54b7752f61ad5f5ee
Eshipillah Pasili Samson <sameshipilla94@gmail.com>
2025-12-07 16:41:21 +0300

    Correctly configure links on the sign-in page

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,6 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('sign-in', views.SignInTemplateView.as_view(), name='sign_in'),
-    path('sign-up', views.SignUpTemplateView.as_view(), name='sign-up'),
-    path('forgot-password', views.ForgotPasswordTemplateView.as_view(), name='forgot-password'),
+    path('sign-up', views.SignUpTemplateView.as_view(), name='sign_up'),
+    path('forgot-password', views.ForgotPasswordTemplateView.as_view(), name='forgot_password'),
 ]
