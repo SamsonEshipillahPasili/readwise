15f4e7cfdfc9cfc9d19da1d77d9443f36224e52b
Eshipillah Pasili Samson <sameshipilla94@gmail.com>
2025-12-15 19:46:37 +0300

    Removed unnecessary test code

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,8 +1,3 @@
 from django.test import TestCase
 from . import models
 
-class CreateAccountRequestTest(TestCase):
-
-    def test_is_valid(self) -> None:
-        req1 = models.CreateAccountRequest.objects.create()
-
