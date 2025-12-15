7b8823382fd7d22b0cd429b04e7778d7353e5e41
Eshipillah Pasili Samson <sameshipilla94@gmail.com>
2025-12-15 20:26:44 +0300

    Add a test to verify the CreateAccountRequestManager.test_create_account works as expected

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from . import managers
 from . import models
+from faker import Faker
 
+class CreateAccountRequestManagerTest(TestCase):
+
+    def setUp(self) -> None:
+        self.fake = Faker()
+    
+    def test_create_account(self) -> None:
+        request = models.CreateAccountRequest.objects.create_request(
+            first_name=self.fake.first_name(),
+            last_name=self.fake.last_name(),
+            username=self.fake.user_name(),
+            email=self.fake.email(),
+            password=self.fake.password(10)
+        )
+
+        saved_request = models.CreateAccountRequest.objects.get(pk=request.id)
+        self.assertEqual(request.first_name, saved_request.first_name)
+        self.assertEqual(request.last_name, saved_request.last_name)
+        self.assertEqual(request.username, saved_request.username)
+        self.assertEqual(request.email, saved_request.email)
+        self.assertEqual(request.password, saved_request.password)
