9a7d8d68a292dc4f6ba2af5dc2d07acab13ae3cc
Eshipillah Pasili Samson <sameshipilla94@gmail.com>
2025-12-15 14:41:28 +0300

    Add the CreateAccountRequest model

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,25 @@
+from uuid import uuid4
+
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from readwise.models import TimestampedModel
 
 class ReadWiseUser(AbstractUser):
     """
     This is the custom user model for all ReadWise users.
     """
     ...
+
+class CreateAccountRequest(TimestampedModel):
+    """
+    Represents the information the user sends when they 
+    create an account before email verification.
+    """
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    username = models.CharField(max_length=255)
+    email = models.EmailField()
+    password = models.CharField(max_length=50)
+    token = models.UUIDField(default=uuid4)
+    is_token_used = models.BooleanField(default=False)
