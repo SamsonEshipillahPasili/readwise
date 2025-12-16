0a38faa7c5ea7306c725957f6cd438c8c23f30aa
Eshipillah Pasili Samson <sameshipilla94@gmail.com>
2025-12-16 14:42:50 +0300

    Add validators to check the password and confirm_password fields

--- accounts/forms.py
+++ accounts/forms.py
@@ -7,3 +7,19 @@ class CreateAccountRequestForm(forms.Form):
     email = forms.EmailField()
     password = forms.CharField(max_length=50)
     confirm_password = forms.CharField(max_length=50)
+
+    def clean_password(self) -> str:
+        password = self.cleaned_data.get('password', '')
+        if len(password) < 8:
+            raise forms.ValidationError('Password should have a minimum of 8 characters.')
+        return password
+    
+    def clean_confirm_password(self) -> str:
+        password = self.cleaned_data.get('password', '')
+        confirm_password = self.cleaned_data.get('confirm_password', '')
+        
+        if password != confirm_password:
+            raise forms.ValidationError('The passwords do not match.')
+        
+        return confirm_password
+
