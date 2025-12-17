95e90fde4535947da22572f608cd7f143fc37e6b
Eshipillah Pasili Samson <sameshipilla94@gmail.com>
2025-12-17 11:09:18 +0300

    Add test cases for the form: CreateAccountRequestForm

--- accounts/tests.py
+++ accounts/tests.py
@@ -4,6 +4,7 @@ from django.test import TestCase
 from django.utils import timezone
 
 from . import models
+from .forms import CreateAccountRequestForm
 from faker import Faker
 
 class CreateAccountRequestManagerTest(TestCase):
@@ -63,3 +64,80 @@ class CreateAccountRequestTest(TestCase):
 
         # the request should be invalid.
         self.assertFalse(self.request.is_valid())
+
+
+class CreateAccountRequestFormTest(TestCase):
+
+    def setUp(self) -> None:
+        self.fake = Faker()
+
+        self.valid_data = {
+            "first_name": self.fake.first_name(),
+            "last_name": self.fake.last_name(),
+            "username": self.fake.user_name(),
+            "email": self.fake.email(),
+            "password": "strongpass123",
+            "confirm_password": "strongpass123",
+        }
+
+    def test_form_valid(self):
+        form = CreateAccountRequestForm(data=self.valid_data)
+        self.assertTrue(form.is_valid())
+
+    def test_password_too_short(self):
+        data = self.valid_data.copy()
+        data["password"] = "short"
+        data["confirm_password"] = "short"
+
+        form = CreateAccountRequestForm(data=data)
+
+        self.assertFalse(form.is_valid())
+        self.assertIn("password", form.errors)
+        self.assertEqual(
+            form.errors["password"][0],
+            "Password should have a minimum of 8 characters."
+        )
+
+    def test_passwords_do_not_match(self):
+        data = self.valid_data.copy()
+        data["confirm_password"] = "differentpass123"
+
+        form = CreateAccountRequestForm(data=data)
+
+        self.assertFalse(form.is_valid())
+        self.assertIn("confirm_password", form.errors)
+        self.assertEqual(
+            form.errors["confirm_password"][0],
+            "The passwords do not match."
+        )
+
+    def test_duplicate_email(self):
+        models.ReadWiseUser.objects.create(
+            username="existing_user",
+            email=self.valid_data["email"]
+        )
+
+        form = CreateAccountRequestForm(data=self.valid_data)
+
+        self.assertFalse(form.is_valid())
+        self.assertIn("email", form.errors)
+        self.assertEqual(
+            form.errors["email"][0],
+            "This email is taken."
+        )
+
+    def test_duplicate_username(self):
+        models.ReadWiseUser.objects.create(
+            username=self.valid_data["username"],
+            email="other@email.com"
+        )
+
+        form = CreateAccountRequestForm(data=self.valid_data)
+
+        self.assertFalse(form.is_valid())
+        self.assertIn("username", form.errors)
+        self.assertEqual(
+            form.errors["username"][0],
+            "Username is taken."
+        )
+
