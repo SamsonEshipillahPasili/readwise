12ec51701706d78512bad33daefb8423514fb629
Eshipillah Pasili Samson <sameshipilla94@gmail.com>
2026-01-12 21:09:41 +0300

    Remove the chat page

--- core/urls.py
+++ core/urls.py
@@ -9,7 +9,6 @@ urlpatterns = [
     path('add-shelf', views.AddShelfView.as_view(), name='add_shelf'),
     path('wishlist', views.WishlistTemplateView.as_view(), name='wishlist'),
     path('friends', views.FriendsTemplateView.as_view(), name='friends'),
-    path('chat', views.ChatTemplateView.as_view(), name='chat'),
     path('profile', views.ProfileTemplateView.as_view(), name='profile'),
     path('settings', views.SettingsTemplateView.as_view(), name='settings'),
 ]
--- core/views.py
+++ core/views.py
@@ -39,14 +39,6 @@ class FriendsTemplateView(LoginRequiredMixin, TemplateView):
         ctx['menu'] = 'friends'
         return ctx
 
-class ChatTemplateView(LoginRequiredMixin, TemplateView):
-    template_name = 'core/chat.html'
-
-    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
-        ctx = super().get_context_data(**kwargs)
-        ctx['menu'] = 'chat'
-        return ctx
-
 class ProfileTemplateView(LoginRequiredMixin, TemplateView):
     template_name = 'core/profile.html'
 
